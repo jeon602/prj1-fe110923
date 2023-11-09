847c88378e30740a638b60caed851236a98d96d5
jeon602 <731aa@naver.com>
2023-11-09 11:13:08 +0900

    package.json에 proxy : http://localhost:8080 추가

---
+++ src/component/NavBar.js
@@ -0,0 +1,15 @@
+import { Button, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+export function NavBar() {
+  const navigate = useNavigate();
+
+  return (
+    <Flex>
+      <Button onClick={() => navigate("/")}>Home</Button>
+      <Button onClick={() => navigate("/write")}>Write</Button>
+    </Flex>
+  );
+}
+//    <Button onClick={() => navigate("/")}>Home</Button> 여기서 홈 버튼 클릭하면 원래 사이트 .
+//<Button onClick={() => navigate("/write")}>Write</Button>write 클릭하면
