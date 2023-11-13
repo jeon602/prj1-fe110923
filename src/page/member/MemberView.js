553061ea3085bafd7c56fb312910d7af96a6d146
jeon602 <731aa@naver.com>
2023-11-13 09:40:07 +0900

    member's infomation check~ing coding~

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,8 +1,34 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Box, FormControl, FormLabel, Input, Spinner } from "@chakra-ui/react";
+import axios from "axios";
 
 export function MemberView() {
+  const [member, setMember] = useState(null);
   // /member ?id = userid
   const [params] = useSearchParams();
+  useEffect(() => {
+    axios
+      .get("/api/member?" + params.toString())
+      .then((response) => setMember(response.data));
+  }, []);
 
-  return <div>{params.get("id")}님 회원 정보 보기</div>;
+  if (member === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <h1>{member.id}님의 정보</h1>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input type="text" value={member.email} readOnly />
+      </FormControl>
+    </Box>
+  );
 }
