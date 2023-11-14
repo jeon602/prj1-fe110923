d553ac4a0a0af22da1ab2e6915dfce82ca48abbc
jeon602 <731aa@naver.com>
2023-11-14 10:14:23 +0900

    글 작성 시 writer input 삭제

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -14,7 +14,7 @@ import { useNavigate } from "react-router-dom";
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [writer, setWriter] = useState("");
+  // const [writer, setWriter] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const toast = useToast();
   const navigate = useNavigate();
@@ -26,7 +26,7 @@ export function BoardWrite() {
       .post("/api/board/add", {
         title,
         content,
-        writer,
+        // writer,
       })
       .then(() => {
         toast({
@@ -67,13 +67,13 @@ export function BoardWrite() {
             onChange={(e) => setContent(e.target.value)}
           ></Textarea>
         </FormControl>
-        <FormControl>
-          <FormLabel>작성자</FormLabel>
-          <Input
-            value={writer}
-            onChange={(e) => setWriter(e.target.value)}
-          ></Input>
-        </FormControl>
+        {/*<FormControl>*/}
+        {/*  <FormLabel>작성자</FormLabel>*/}
+        {/*  <Input*/}
+        {/*    value={writer}*/}
+        {/*    onChange={(e) => setWriter(e.target.value)}*/}
+        {/*  ></Input>*/}
+        {/*</FormControl>*/}
         <Button
           isDisabled={isSubmitting}
           onClick={handleSubmit}
