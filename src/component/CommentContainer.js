90554818e184f81470634612f5a4b98ef9d2fa1a
jeon602 <731aa@naver.com>
2023-11-15 15:02:18 +0900

    댓글 삭제 시 모달로 확인

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -6,13 +6,22 @@ import {
   CardHeader,
   Flex,
   Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   StackDivider,
   Text,
   Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
@@ -31,18 +40,7 @@ function CommentForm({ boardId, isSubmitting, onSubmit }) {
   );
 }
 
-function CommentList({ ComentList }) {
-  const [commentList, setCommentList] = useState(false);
-
-  useEffect(() => {
-    const params = new URLSearchParams();
-    params.set("id", boardId);
-
-    axios
-      .get("/api/comment/list?" + params)
-      .then((response) => setCommentList(response.data));
-  }, [isSubmitting]);
-
+function CommentList({ commentList, onDeleteModalOpen, isSubmitting }) {
   return (
     <Card>
       <CardHeader>
@@ -50,16 +48,25 @@ function CommentList({ ComentList }) {
       </CardHeader>
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
-          {/* TODO: 댓글 작성 후 re render */}
           {commentList.map((comment) => (
-            <Box>
+            <Box key={comment.id}>
               <Flex justifyContent="space-between">
                 <Heading size="xs">{comment.memberId}</Heading>
                 <Text fontSize="xs">{comment.inserted}</Text>
               </Flex>
-              <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
-                {comment.comment}
-              </Text>
+              <Flex justifyContent="space-between" alignItems="center">
+                <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                  {comment.comment}
+                </Text>
+                <Button
+                  isDisabled={isSubmitting}
+                  onClick={() => onDeleteModalOpen(comment.id)}
+                  size="xs"
+                  colorScheme="red"
+                >
+                  <DeleteIcon />
+                </Button>
+              </Flex>
             </Box>
           ))}
         </Stack>
@@ -70,6 +77,21 @@ function CommentList({ ComentList }) {
 
 export function CommentContainer({ boardId }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [id, setId] = useState(0);
+  const [commentList, setCommentList] = useState([]);
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      const params = new URLSearchParams();
+      params.set("id", boardId);
+
+      axios
+        .get("/api/comment/list?" + params)
+        .then((response) => setCommentList(response.data));
+    }
+  }, [isSubmitting]);
 
   function handleSubmit(comment) {
     setIsSubmitting(true);
@@ -79,6 +101,23 @@ export function CommentContainer({ boardId }) {
       .finally(() => setIsSubmitting(false));
   }
 
+  function handleDelete() {
+    // console.log(id + "번 댓글 삭제");
+    // TODO: 모달, then, catch, finally
+
+    setIsSubmitting(true);
+    axios.delete("/api/comment/" + id).finally(() => {
+      onClose();
+      setIsSubmitting(false);
+    });
+  }
+
+  function handleDeleteModalOpen(id) {
+    // id 를 어딘가 저장
+    setId(id);
+    // 모달 열기
+    onOpen();
+  }
   return (
     <Box>
       <CommentForm
@@ -86,7 +125,156 @@ export function CommentContainer({ boardId }) {
         isSubmitting={isSubmitting}
         onSubmit={handleSubmit}
       />
-      <CommentList boardId={boardId} ComentList={CommentList}/>
+      <CommentList
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        commentList={commentList}
+        onDeleteModalOpen={handleDeleteModalOpen}
+      />
+
+      {/* 삭제 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button
+              isDisabled={isSubmitting}
+              onClick={handleDelete}
+              colorScheme="red"
+            >
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
+//my code understage
+// import {
+//   Box,
+//   Button,
+//   Card,
+//   CardBody,
+//   CardHeader,
+//   Flex,
+//   Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay,
+//   Stack,
+//   StackDivider,
+//   Text,
+//   Textarea, useDisclosure,
+// } from "@chakra-ui/react";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+//
+// function CommentForm({ boardId, isSubmitting, onSubmit }) {
+//   const [comment, setComment] = useState("");
+//
+//   function handleSubmit() {
+//     onSubmit({ boardId, comment });
+//   }
+//
+//   return (
+//     <Box>
+//       <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+//       <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+//         쓰기
+//       </Button>
+//     </Box>
+//   );
+// }
+//
+// function CommentList({ CommentList, onDelete, isSubmitting }) {
+//   const [CommentList, setCommentList] = useState(false);
+//
+//   useEffect(() => {
+//     const params = new URLSearchParams();
+//     params.set("id", boardId);
+//
+//     axios
+//       .get("/api/comment/list?" + params)
+//       .then((response) => setCommentList(response.data));
+//   }, [isSubmitting]);
+//
+//   return (
+//     <Card>
+//       <CardHeader>
+//         <Heading size="md">댓글 리스트</Heading>
+//       </CardHeader>
+//       <CardBody>
+//         <Stack divider={<StackDivider />} spacing="4">
+//           {/* TODO: 댓글 작성 후 re render */}
+//           {commentList.map((comment) => (
+//             <Box>
+//               <Flex justifyContent="space-between">
+//                 <Heading size="xs">{comment.memberId}</Heading>
+//                 <Text fontSize="xs">{comment.inserted}</Text>
+//               </Flex>
+//               <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+//                 {comment.comment}
+//               </Text>
+//             </Box>
+//           ))}
+//         </Stack>
+//       </CardBody>
+//     </Card>
+//   );
+// }
+//
+// export function CommentContainer({ boardId }) {
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   const [CommentList, setCommentList]=useState();
+//   const {isOpen,onClose,onOpen }=useDisclosure();
+//
+//   function handleSubmit(comment) {
+//     setIsSubmitting(true);
+//     axios.delete()
+//
+//     axios
+//       .post("/api/comment/add", comment)
+//       .finally(() => setIsSubmitting(false));
+//
+//   }
+//
+//   function handleDelete() {
+//
+//   }
+//
+//   function handleDeleteModalOpen(id) {
+//     setId(id);
+//     // 아이디를 어딘가에 저장하고 , 모달을 열고 onOpen
+//     onOpen();
+//   }
+//
+//   return (
+//     <Box>
+//       <CommentForm
+//         boardId={boardId}
+//         isSubmitting={isSubmitting}
+//         onSubmit={handleSubmit}
+//         onDeleteModalOpen={handleDeleteModalOpen}
+//       />
+//       <CommentList boardId={boardId} ComentList={CommentList}/>
+//       <Modal isOpen={isOpen} onClose={onClose}>
+//         <ModalOverlay />
+//         <ModalContent>
+//           <ModalHeader>삭제 확인</ModalHeader>
+//           <ModalCloseButton />
+//           <ModalBody>삭제 하시겠습니까?</ModalBody>
+//
+//           <ModalFooter>
+//             <Button onClick={onClose}>닫기</Button>
+//             <Button onClick={handleDelete} colorScheme="red">
+//               삭제
+//             </Button>
+//           </ModalFooter>
+//         </ModalContent>
+//       </Modal>
+//     </Box>
+//   );
+// }
