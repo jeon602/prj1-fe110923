7506148ca37beb55a121d75df5ecbc5cb1c6749f
jeon602 <731aa@naver.com>
2023-11-15 11:49:21 +0900

    새 댓글 쓰는 중 is Submitting을 CommentContrainer로 이동 새 댓글 작 성 후에 댓글리스트 기능

---
+++ src/component/CommentContainer.js
@@ -0,0 +1,92 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function CommentForm({ boardId, isSubmitting, onSubmit }) {
+  const [comment, setComment] = useState("");
+
+  function handleSubmit() {
+    onSubmit({ boardId, comment });
+  }
+
+  return (
+    <Box>
+      <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+      <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+        쓰기
+      </Button>
+    </Box>
+  );
+}
+
+function CommentList({ ComentList }) {
+  const [commentList, setCommentList] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set("id", boardId);
+
+    axios
+      .get("/api/comment/list?" + params)
+      .then((response) => setCommentList(response.data));
+  }, [isSubmitting]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {/* TODO: 댓글 작성 후 re render */}
+          {commentList.map((comment) => (
+            <Box>
+              <Flex justifyContent="space-between">
+                <Heading size="xs">{comment.memberId}</Heading>
+                <Text fontSize="xs">{comment.inserted}</Text>
+              </Flex>
+              <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                {comment.comment}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+}
+
+export function CommentContainer({ boardId }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit(comment) {
+    setIsSubmitting(true);
+
+    axios
+      .post("/api/comment/add", comment)
+      .finally(() => setIsSubmitting(false));
+  }
+
+  return (
+    <Box>
+      <CommentForm
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        onSubmit={handleSubmit}
+      />
+      <CommentList boardId={boardId} ComentList={CommentList}/>
+    </Box>
+  );
+}
