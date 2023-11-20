eb39929c6745077f949ee0b581f1fe6de555f8cb
jeon602 <731aa@naver.com>
2023-11-20 09:26:03 +0900

    검색 input , button 추가

---
+++ src/page/board/BoardList.js
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Input,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { ChatIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+
+function Pagination({ pageInfo }) {
+  const pageNumbers = [];
+
+  const navigate = useNavigate();
+
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box>
+      {pageInfo.prevPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.prevPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </Button>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <Button
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          onClick={() => navigate("/?p=" + pageNumber)}
+        >
+          {pageNumber}
+        </Button>
+      ))}
+
+      {pageInfo.nextPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.nextPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </Button>
+      )}
+    </Box>
+  );
+}
+
+function SearchComponent() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSearch() {
+    // /?k=keyword
+    const params = new URLSearchParams();
+    params.set("k", keyword);
+
+    navigate("/?" + params);
+  }
+
+  return (
+    <Flex>
+      <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <Button onClick={handleSearch}>검색</Button>
+    </Flex>
+  );
+}
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+  const [pageInfo, setPageInfo] = useState(null);
+
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+//
+  useEffect(() => {
+    axios.get("/api/board/list?" + params).then((response) => {
+      setBoardList(response.data.boardList);
+      setPageInfo(response.data.pageInfo);
+    });
+  }, [location]);
+
+  if (boardList === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <h1>게시물 목록</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>
+                <FontAwesomeIcon icon={faHeart} />
+              </Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr
+                _hover={{
+                  cursor: "pointer",
+                }}
+                key={board.id}
+                onClick={() => navigate("/board/" + board.id)}
+              >
+                <Td>{board.id}</Td>
+                <Td>{board.countLike != 0 && board.countLike}</Td>
+                <Td>
+                  {board.title}
+                  {board.countComment > 0 && (
+                    <Badge>
+                      <ChatIcon />
+                      {board.countComment}
+                    </Badge>
+                  )}
+                </Td>
+                <Td>{board.nickName}</Td>
+                <Td>{board.ago}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+
+      <SearchComponent />
+      <Pagination pageInfo={pageInfo} />
+    </Box>
+  );
+}
