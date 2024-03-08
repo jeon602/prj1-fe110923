e405878e2abf250c28105220e73a031a549f981b
yeon <731aa@naver.com>
2024-03-08 21:41:03 +0900

    다마고찌 프로젝트 개발일정 html파일

---
+++ src/myprofileTechStack.js
@@ -0,0 +1,14 @@
+import {Center, Table} from "@chakra-ui/react";
+
+export function myprofileTechStack(){
+
+    return (
+        <Table>
+            <th>wjsgmlsuds</th>
+            <th>wjsgmlsuds</th>
+            <th>wjsgmlsuds</th>
+
+        </Table>
+
+    )
+}
